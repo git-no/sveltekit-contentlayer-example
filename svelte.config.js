8019a8e935f41183b7a7a313e56fbb666279079e
Dr. No <mo+ita-git@depeche.email>
2023-09-01 17:36:33 +0200

    Simpler approach of contentlayer alias

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		alias: {
+			'contentlayer/generated':'./.contentlayer/generated'
+		},
 		prerender: {
 			handleHttpError: 'fail'
 		}
